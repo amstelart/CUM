a16d08c1a927feb0064e8824cbdc6718fb3bc644
amstelart <amsteldroid@gmail.com>
2019-01-15 16:33:52 +0300

    Update 2019-01-15T13:33:51.305Z

--- js/script.js
+++ js/script.js
@@ -267,41 +267,41 @@ $( document ).ready(function() {
     });
   };
 
-  $("#main-carousel").owlCarousel({
-    items: 3,
-    nav: true,
-    loop: true,
-    autoplay: true,
-    margin: 30,
-    dots: true,
-    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    center: false,
-    responsive : {
-      0 : {
-        items: 1,
-        nav: true,
-        loop: true,
-        center: true,
-      },
-      480 : {
-        items: 2,
-      },
-      768 : {
-        items: 2,
-      },
-      992 : {
-        items: 2,
-      },
-      1200 : {
-        items: 3,
-        center: true
-      },
-      1800 : {
-        items: 3,
-        center: true
-      }
-    }
-  });
+  // $("#main-carousel").owlCarousel({
+  //   items: 3,
+  //   nav: true,
+  //   loop: true,
+  //   autoplay: true,
+  //   margin: 30,
+  //   dots: true,
+  //   navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  //   center: false,
+  //   responsive : {
+  //     0 : {
+  //       items: 1,
+  //       nav: true,
+  //       loop: true,
+  //       center: true,
+  //     },
+  //     480 : {
+  //       items: 2,
+  //     },
+  //     768 : {
+  //       items: 2,
+  //     },
+  //     992 : {
+  //       items: 2,
+  //     },
+  //     1200 : {
+  //       items: 3,
+  //       center: true
+  //     },
+  //     1800 : {
+  //       items: 3,
+  //       center: true
+  //     }
+  //   }
+  // });
 
   $(".brand-gallery").owlCarousel({
     items: 4,
@@ -309,7 +309,30 @@ $( document ).ready(function() {
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     margin: 30,
     loop: true,
-    dots: false
+    dots: false,
+    responsive : {
+      0 : {
+        items: 2,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 3,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 4,
+      },
+      1200 : {
+        items: 4,
+      },
+      1800 : {
+        items: 4,
+      }
+    }
   });
 
 });
